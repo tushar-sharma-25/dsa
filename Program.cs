87453955e3647e9a19a2b7243716383fb20830e6
tushar-sharma-25 <tusharsharma2509@gmail.com>
2024-11-12 22:30:50 +0530

    restructure project

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Problems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input  = new int[] { 1,2,3,2,2 };
+
+            Console.WriteLine(TotalFruit(input));
+        }
+
+        public static int TotalFruit(int[] input)
+        {
+
+            Dictionary<int, int> treeMap = new Dictionary<int, int>();
+            int end =0, start =0;
+            int maxFruits= 0;
+
+            while(end<input.Length){
+                //if present in map
+                if(treeMap.ContainsKey(input[end])){
+                    treeMap[input[end]] += 1;
+                }
+                    
+                // else
+                else{
+                    if(treeMap.Count < 2){
+                        treeMap.Add(input[end], 1);
+                    }
+                    // we have encountered a new unique fruit
+                    else{
+                        int localMax = treeMap.Values.Sum();
+                        maxFruits = Math.Max(maxFruits, localMax);
+                        treeMap.Remove(input[start]);
+                        treeMap.Add(input[end], 1);
+                        start++;
+                    }
+                }
+                end++;
+            }
+            return maxFruits;
+
+        }
+    }
+}
